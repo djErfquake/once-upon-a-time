6642b3e356ead9a7b89dced85273b0ef0291903a
clevally <clevally@roto.com>
2019-06-13 12:38:38 -0400

    initial commit

---
+++ app/ouat.js
@@ -0,0 +1,150 @@
+const COUNTDOWN_TIME = 1;
+const RESET_TIME = 0.7;
+
+const CARD_COLORS = {
+    "aspect": "#ff6b6b",
+    "character": "#1dd1a1",
+    "event": "#48dbfb",
+    "place": "#feca57",
+    "thing": "#5f27cd",
+    "ending": "#222f3e"
+};
+
+const CARD_ICONS = {
+    "aspect": "fa-ruler",
+    "character": "fa-user",
+    "event": "fa-bolt",
+    "place": "fa-map-marker",
+    "thing": "fa-socks",
+    "ending": "fa-hourglass-end"
+};
+
+const socket = io();
+
+let timer = $('.timer-bar'); timer.active = false;
+let cardContainer = $('.card-container');
+let endingContainer = $('.ending-container');
+let cards = [];
+let newCardIndex = 0;
+let winner = false;
+let turnOverButton = $('.turn-over-button'); turnOverButton.addClass('hidden'); turnOverButton.active = false;
+turnOverButton.on('click', function() {
+    turnOverButton.addClass('hidden');
+    turnOverButton.active = false;
+    socket.emit('TURN_OVER');
+});
+
+
+timer.addClass('clear');
+timer.start = function() {
+    timer.active = true;
+    timer.css('transition', `transform ${COUNTDOWN_TIME}s linear`);
+    timer.addClass('timer-active');
+    timer.removeClass('clear');
+    timer.timeout = setTimeout(function() {
+        timer.css('transition', `transform ${RESET_TIME}s ease-out`);
+        timer.removeClass('timer-active');
+        timer.addClass('clear');
+        timer.active = false;
+    }, COUNTDOWN_TIME * 1000);
+};
+
+function addCard(card) {
+    let index = newCardIndex;
+    card.index = index;
+    cards.push(card)
+    newCardIndex++;
+
+    let cardName = card.name;
+    let cardInterrupt = card.interruptible;
+    let cardDiv = `<div class="word-card btn block text-white shadow card-${index}">`;
+    cardDiv += `<i class="type-icon fa ${CARD_ICONS[card.cardtype]}"></i>`;
+    cardDiv += `${cardName}`;
+    if (card.interruptible) {
+        cardDiv += `<i class="interrupt-icon fa fa-angle-double-up"></i>`;
+    }
+    cardDiv += `</div>`
+    cardContainer.append(cardDiv);
+
+    let cardDOM = $(`.card-${index}`);
+    cardDOM.css('background-color', CARD_COLORS[card.cardtype]);
+    cardDOM.on('click', function() {
+        if (!timer.active && (turnOverButton.active || cardInterrupt)) {
+
+            timer.start();
+
+            cardDOM.addClass('hidden');
+            for (let i = 0; i < cards.length; i++) {
+                if (cards[i].name == cardName) {
+                    socket.emit('CARD_PLAYED', cards[i]);
+                    cards.splice(i, 1);
+                    break;
+                }
+            }
+            console.log(`${cards.length} cards left.`);
+        }
+    });
+};
+
+function addEnding(card) {
+
+    let cardName = card.name;
+    let cardDiv = `<div class="word-card btn block text-white shadow card-ending">`;
+    cardDiv += `<i class="type-icon fa ${CARD_ICONS[card.cardtype]}"></i>`;
+    cardDiv += `${cardName}`;
+    if (card.interruptible) {
+        cardDiv += `<i class="interrupt-icon fa fa-angle-double-up"></i>`;
+    }
+    cardDiv += `</div>`
+    endingContainer.append(cardDiv);
+    let cardDOM = $(`.card-ending`);
+    cardDOM.css('background-color', CARD_COLORS[card.cardtype]);
+    cardDOM.on('click', function() {
+        console.log(`${!timer.active} && ${turnOverButton.active} && ${cards.length} == 0`);
+        if (!timer.active && turnOverButton.active && cards.length == 0) {
+            winner = true;
+            Swal.fire(
+                'You win!',
+                'You used all of your story elements before anyone else! Refresh the page to restart.',
+                'success'
+            );
+            socket.emit('GAME_OVER');
+        }
+    });
+};
+
+
+
+socket.emit('CARD_REQUEST');
+socket.on('CARDS', function(cardData) {
+    console.log("got cards", cardData);
+    for (let i = 0; i < cardData.length - 1; i++) {
+        addCard(cardData[i]);
+    }
+    addEnding(cardData[cardData.length - 1]);
+});
+
+socket.on('CARD_PLAYED', function(card) {
+    Swal.fire(`${card.name}`);
+    turnOverButton.addClass('hidden');
+    turnOverButton.active = false;
+});
+
+socket.on('NEW_CARD', function(newCard) {
+    addCard(newCard);
+});
+
+socket.on('YOUR_TURN', function() {
+    turnOverButton.removeClass('hidden');
+    turnOverButton.active = true;
+});
+
+socket.on('GAME_OVER', function() {
+    if (!winner) {
+        Swal.fire(
+            'You lost!',
+            'Try to used all of your story elements before anyone else next time! Refresh the page to restart.',
+            'error'
+        );
+    }
+});
---
+++ index.js
@@ -0,0 +1,158 @@
+const NUM_OF_CARDS = 10;
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const port = process.env.PORT || 9713;
+const io = require('socket.io')(server);
+const _ = require('underscore');
+const fs = require('fs');
+
+//////////////////////
+// START SERVER
+//////////////////////
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+// serve these pages
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '', 'app', 'ouat.html'));
+});
+
+// start listening for requests
+server.listen(port, () => { console.log(`Listening on port ${port}`); });
+
+
+//////////////////////
+// SOCKET IO
+//////////////////////
+
+let sockets = [];
+let currentSocketIndex = 0;
+
+io.on('connection', function(socket) {
+
+    socket.index = currentSocketIndex;
+    socket.activePlayer = false;
+    sockets.push(socket);
+    console.log(`user ${currentSocketIndex} connected`);
+    currentSocketIndex++;
+    
+    // setup for disconnect
+    socket.on('disconnect', function() {
+
+        // delete player
+        let socketsIndex = 0;
+        for (let i = 0; i < sockets.length; i++) {
+            if (sockets[i].index == socket.index) {
+                socketsIndex = i;
+                sockets.splice(socketsIndex, 1);
+                break;
+            } 
+        }
+        console.log(`user ${socket.index} disconnected. ${sockets.length} sockets left.`);
+
+        // set new active player if necessary
+        if (socket.activePlayer && sockets.length > 0) {
+            if (socketsIndex >= sockets.length) { socketsIndex = 0; }
+
+            console.log(`setting active player to ${socketsIndex}/${sockets.length}`);
+
+            sockets[socketsIndex].activePlayer = true;
+            sockets[socketsIndex].emit('YOUR_TURN');
+        }
+    });
+
+    // asking for cards
+    socket.on('CARD_REQUEST', function() {
+        socket.cards = [];
+        for (let i = 0; i < NUM_OF_CARDS - 1; i++) {
+            socket.cards.push(cards.randomCard());         
+        }
+        socket.cards.sort(function(a, b) { return a.cardtype.localeCompare(b.cardtype); });
+
+        socket.cards.push(cards.randomEnding());
+        socket.emit('CARDS', socket.cards);
+    });
+
+    // set start player
+    if (sockets.length == 1) {
+        socket.activePlayer = true;
+        socket.emit('YOUR_TURN');
+    }
+
+    // let all players know when a card is played
+    socket.on('CARD_PLAYED', function(card) {
+        for (let i = 0; i < sockets.length; i++) {
+            if (sockets[i] != socket) {
+                sockets[i].emit('CARD_PLAYED', card);
+                sockets[i].activePlayer = false;
+            }    
+        }
+
+        socket.activePlayer = true;
+        socket.emit('YOUR_TURN');
+    });
+
+    // listen for turn switching
+    socket.on('TURN_OVER', function() {
+
+        // let new player know it is their turn
+        for (let i = 0; i < sockets.length; i++) {
+            let socketIndex = i;
+            if (sockets[socketIndex].activePlayer) {
+                socketIndex++; if (socketIndex >= sockets.length) { socketIndex = 0; }
+                sockets[socketIndex].activePlayer = true;
+                sockets[socketIndex].emit('YOUR_TURN');
+                sockets[i].activePlayer = false;
+                break;
+            }
+        }
+        
+        // give old player a card for passing
+        let newCard = cards.randomCard();
+        socket.cards.push(newCard);
+        socket.emit('NEW_CARD', newCard);
+    });
+
+    // listen for game end
+    socket.on('GAME_OVER', function() {
+        for (let i = 0; i < sockets.length; i++) {
+            sockets[i].emit('GAME_OVER');
+        }
+    });
+
+});
+
+
+//////////////////////
+// LOAD CARDS
+//////////////////////
+let cards = JSON.parse(fs.readFileSync('cards.json'));
+cards = _.groupBy(cards, 'cardtype');
+cards.allCards = cards.aspect.concat(cards.character).concat(cards.event).concat(cards.place).concat(cards.thing);
+cards.randomCard = function() {
+    let cardIndex = Math.floor(Math.random()*cards.allCards.length);
+    let card = cards.allCards.splice(cardIndex, 1)[0];
+    //console.log(`taking card ${cardIndex}`, cards);
+    return card;
+};
+cards.randomEnding = function() {
+    let cardIndex = Math.floor(Math.random()*cards.ending.length);
+    let card = cards.ending.splice(cardIndex, 1)[0];
+    return card;
+}
+//console.log("themes", cards.theme);
+//console.log("random ending", cards.randomEnding());
+//console.log("cards", cards);
+//console.log("random card", cards.randomCard());
+
+/*
+// TODO:
+make timer work
+
+put on heroku
+test
+
+*/
